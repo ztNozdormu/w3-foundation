3af6db6318c4f739cb57f1401e8cdb71ee253311
真土 <2307631397@qq.com>
2023-08-14 15:59:27 +0800

    配置文件修改，代码生成测试

---
+++ w3-framework/w3-spring-boot-starter-clickhouse/src/main/java/com/w3/framework/clickhouse/config/CkProperties.java
@@ -0,0 +1,35 @@
+//package com.w3.framework.clickhouse.config;
+//
+//import lombok.Data;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+///**
+// * jdbc基本配置参数
+// */
+//@ConfigurationProperties(prefix = "spring.datasource.click")
+//@Data
+//public class CkProperties {
+//
+//    public String address;
+//
+//    public String username;
+//
+//    public String password;
+//
+//    public String dbName;
+//
+//    public int socketTimeout;
+//
+//    private String driverClassName;
+//
+//    private String url;
+//
+//    private int initialSize;
+//
+//    private int maxActive;
+//
+//    private int minIdle;
+//
+//    private int maxWait;
+//
+//}
--- w3-framework/w3-spring-boot-starter-clickhouse/src/main/java/com/w3/framework/clickhouse/config/ClickHouseConfig.java
+++ w3-framework/w3-spring-boot-starter-clickhouse/src/main/java/com/w3/framework/clickhouse/config/ClickHouseConfig.java
@@ -1,65 +1,57 @@
-package com.w3.framework.clickhouse.config;
-
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import ru.yandex.clickhouse.ClickHouseConnection;
-import ru.yandex.clickhouse.ClickHouseDataSource;
-import ru.yandex.clickhouse.settings.ClickHouseProperties;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-/**
- * @desc ClickHouse基本配置参数
- * SQL方式执行
- */
-@AutoConfiguration
-@EnableConfigurationProperties({ClickhouseProperties.class})
-@Data
-public class ClickHouseConfig {
-
-    private static ClickHouseDataSource clickHouseDataSource;
-
-    @Bean
-    public ClickHouseDataSource redisCacheConfiguration(ClickhouseProperties clickhouseProperties) {
-        ClickHouseProperties properties = new ClickHouseProperties();
-        properties.setUser(clickhouseProperties.username);
-        properties.setPassword(clickhouseProperties.password);
-        properties.setDatabase(clickhouseProperties.dbName);
-        properties.setSocketTimeout(clickhouseProperties.socketTimeout);
-        clickHouseDataSource = new ClickHouseDataSource(clickhouseProperties.address, properties);
-        return clickHouseDataSource;
-    }
-
-    public static List<Map<String, String>> exeSql(String sql) {
-        System.out.println("cliockhouse 执行sql：" + sql);
-        Connection connection = null;
-        try {
-            connection = clickHouseDataSource.getConnection();
-            assert connection != null;
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery(sql);
-            ResultSetMetaData rsmd = results.getMetaData();
-            List<Map<String, String>> list = new ArrayList<>();
-            while (results.next()) {
-                Map<String, String> row = new HashMap<>();
-                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                    row.put(rsmd.getColumnName(i), results.getString(rsmd.getColumnName(i)));
-                }
-                list.add(row);
-            }
-            return list;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-}
+//package com.w3.framework.clickhouse.config;
+//
+//import lombok.Data;
+//import org.springframework.boot.autoconfigure.AutoConfiguration;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import ru.yandex.clickhouse.ClickHouseDataSource;
+//import ru.yandex.clickhouse.settings.ClickHouseProperties;
+//
+//
+///**
+// * @desc ClickHouse基本配置参数
+// * SQL方式执行
+// */
+//@AutoConfiguration
+//@EnableConfigurationProperties({CkProperties.class})
+//@Data
+//public class ClickHouseConfig {
+//
+////    private static ClickHouseDataSource clickHouseDataSource;
+//
+//    @Bean
+//    public ClickHouseDataSource clickHouseDataSource(CkProperties ckProperties) {
+//        ClickHouseProperties properties = new ClickHouseProperties();
+//        properties.setUser(ckProperties.username);
+//        properties.setPassword(ckProperties.getPassword());
+//        properties.setDatabase(ckProperties.dbName);
+//        properties.setSocketTimeout(ckProperties.socketTimeout);
+////        clickHouseDataSource = new ClickHouseDataSource(clickhouseProperties.address, properties);
+////        return clickHouseDataSource;
+//        return new ClickHouseDataSource(ckProperties.address, properties);
+//    }
+//
+////    public static List<Map<String, String>> exeSql(String sql) {
+////        System.out.println("cliockhouse 执行sql：" + sql);
+////        Connection connection = null;
+////        try {
+////            connection = clickHouseDataSource.getConnection();
+////            assert connection != null;
+////            Statement statement = connection.createStatement();
+////            ResultSet results = statement.executeQuery(sql);
+////            ResultSetMetaData rsmd = results.getMetaData();
+////            List<Map<String, String>> list = new ArrayList<>();
+////            while (results.next()) {
+////                Map<String, String> row = new HashMap<>();
+////                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+////                    row.put(rsmd.getColumnName(i), results.getString(rsmd.getColumnName(i)));
+////                }
+////                list.add(row);
+////            }
+////            return list;
+////        } catch (SQLException e) {
+////            e.printStackTrace();
+////        }
+////        return null;
+////    }
+//}
